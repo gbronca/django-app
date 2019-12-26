bca1200a6e47b5ea2e5661c6fd14149fc985fb80
gbronca <gbronca@gmail.com>
2019-12-26 22:03:07 +0000

    login page created

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from .views import signup
+from django.contrib.auth import views as auth_views
 # from . import views
 
 app_name = 'accounts'
 
 urlpatterns = [
-    path('signup/',signup, name='signup')
-    # path('', views.ProductListView.as_view(), name='products_list'),
-    # path('<pk>', views.ProductDetailView.as_view(), name='product_detail'),
+    path('signup/',signup, name='signup'),
+    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='accounts/logout.html'), name='logout'),
 ]
