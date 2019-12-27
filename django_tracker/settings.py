2d6c50953cae3a4bfb78b3b220b3b9b9514f0841
gbronca <gbronca@gmail.com>
2019-12-27 10:23:49 +0000

    started bugs and features app, navbar in new file

--- django_tracker/settings.py
+++ django_tracker/settings.py
@@ -119,7 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATICFILES_LOCATION = 'static'
 STATIC_URL = '/static/'
-STATICFILES_DIR = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
