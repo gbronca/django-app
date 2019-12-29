75c679a68a58f6a0e0d17eeb4242014f945e480e
gbronca <gbronca@gmail.com>
2019-12-29 18:23:40 +0000

    improved navigation between bugs & features

--- tickets/urls.py
+++ tickets/urls.py
@@ -5,11 +5,12 @@ from . import views
 app_name = 'tickets'
 
 urlpatterns = [
-    path('bugs/', views.BugListView.as_view(template_name='tickets/bugs.html'), name='bugs'),
-    path('features/', views.FeaturesListView.as_view(template_name='tickets/features.html'), name='features'),
+    path('bugs/', views.BugListView.as_view(template_name='tickets/tickets.html'), name='bugs'),
+    path('features/', views.FeaturesListView.as_view(template_name='tickets/tickets.html'), name='features'),
     path('detail/<int:pk>/', views.TicketDetailView.as_view(), name='detail'),
     path('new/', views.TicketCreateView.as_view(), name='new-ticket'),
     path('detail/<int:pk>/update', views.TicketUpdateView.as_view(), name='update'),
     path('detail/<int:pk>/delete', views.TicketDeleteView.as_view(), name='delete'),
-    path('user/<username>/', views.UserBugListView.as_view(template_name='tickets/user_bugs.html'), name='user'),
+    path('user-bugs/<username>/', views.UserBugListView.as_view(template_name='tickets/user_tickets.html'), name='user_bugs'),
+    path('user-features/<username>/', views.UserFeatureListView.as_view(template_name='tickets/user_tickets.html'), name='user_features'),
 ]
--- tickets/views.py
+++ tickets/views.py
@@ -10,35 +10,69 @@ from django.views.generic import (
 from .models import Ticket
 
 class BugListView(ListView):
+    paginate_by = 5
     model = Ticket
-    # template_name = 'tickets/bugs.html'
-    context_object_name = 'bugs'
-    paginate_by = 2
 
     def get_queryset(self, *args, **kwargs):
         return Ticket.objects.filter(issue='Bug').order_by('-date_posted')
 
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in a QuerySet of all the books
+        context['type'] = 'Bugs'
+        is_paginated = True
+        return context
+
 
 class UserBugListView(ListView):
+    paginate_by = 5
     model = Ticket
-    # template_name = 'tickets/bugs.html'
-    context_object_name = 'bugs'
-    paginate_by = 2
 
     def get_queryset(self, *args, **kwargs):
         username = get_object_or_404(User, username=self.kwargs.get('username'))
         return Ticket.objects.filter(issue='Bug', username=username).order_by('-date_posted')
 
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in a QuerySet of all the books
+        context['type'] = 'Bugs'
+        is_paginated = True
+        return context
+
 
 class FeaturesListView(ListView):
+    paginate_by = 5
     model = Ticket
-    # template_name = 'tickets/features.html'
-    context_object_name = 'features'
 
     def get_queryset(self, *args, **kwargs):
         return Ticket.objects.filter(issue='Feature').order_by('-date_posted')
 
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in a QuerySet of all the books
+        context['type'] = 'Features'
+        is_paginated = True
+        return context
 
+
+class UserFeatureListView(ListView):
+    paginate_by = 5
+    model = Ticket
+
+    def get_queryset(self, *args, **kwargs):
+        username = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Ticket.objects.filter(issue='Feature', username=username).order_by('-date_posted')
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in a QuerySet of all the books
+        context['type'] = 'Features'
+        is_paginated = True
+        return context
 
 
 class TicketDetailView(DetailView):
