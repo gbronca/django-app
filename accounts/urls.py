a16edd7df036d0c12a889dd9340cb50b24d50694
gbronca <gbronca@gmail.com>
2019-12-29 16:15:58 +0000

    Reset password functionality added

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,6 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
 from .views import signup, user_profile
 from django.contrib.auth import views as auth_views
-# from . import views
 
 app_name = 'accounts'
 
@@ -11,4 +10,18 @@ urlpatterns = [
                    extra_context={'title':'Login'}), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('profile/', user_profile, name='profile'),
+    path('password-reset/',
+         auth_views.PasswordResetView.as_view(
+                                template_name='accounts/password_reset.html',
+                                success_url = reverse_lazy('accounts:password_reset_done')),
+         name='password_reset'),
+    path('password-reset-confirm/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(template_name='accounts/password_reset_confirm.html'),
+         name='password_reset_confirm'),
+    path('password-reset/done/',
+         auth_views.PasswordResetDoneView.as_view(template_name='accounts/password_reset_done.html'),
+         name='password_reset_done'),
+    path('password-reset-complete/',
+         auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset_complete.html'),
+         name='password_reset_complete'),
 ]
--- django_tracker/urls.py
+++ django_tracker/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
+    # path('', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
     path('',TemplateView.as_view(template_name='index.html'), name='index'),
     path('about/',TemplateView.as_view(template_name='about.html'), name='about'),
