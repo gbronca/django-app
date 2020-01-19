6b238e27c2420773fff82ca67327503f7bc056bd
gbronca <gbronca@gmail.com>
2020-01-19 13:46:33 +0000

    updated checkout css and card validation

--- checkout/forms.py
+++ checkout/forms.py
@@ -8,11 +8,16 @@ class PaymentForm(forms.Form):
     MONTH_CHOICES = [(i, i) for i in range(1, 13)]
     YEAR_CHOICES = [(i, i) for i in range(current_year, current_year + 10)]
     
-    credit_card_number = forms.CharField(
-                         label='Credit Card number',
-                         required=True,
-                         max_length=16)
-    cvv = forms.CharField(label='Security code(CVV)', required=True, max_length=3,min_length=3)
+    name = forms.CharField(label='Name on Credit Card',
+                                       max_length=200)
+    credit_card_number = forms.CharField(label='Credit Card number',
+                                         required=False,
+                                         max_length=19,
+                                         min_length=14)
+    cvv = forms.CharField(label='Security code(CVV)',
+                          required=False,
+                          max_length=4,
+                          min_length=3)
     expiry_month = forms.ChoiceField(label='Month', choices=MONTH_CHOICES, required=False)
     expiry_year = forms.ChoiceField(label='Year', choices=YEAR_CHOICES, required=False)
     stripe_id = forms.CharField(widget=forms.HiddenInput)
--- static/script/ccform.js
+++ static/script/ccform.js
@@ -1,3 +1,13 @@
+$('#id_expiry_month, #id_expiry_year').on('change', function () {
+	// Set the value of a hidden input field for Card
+	$('#expiry-date').val($('#id_expiry_month').val() + '/' + $('#id_expiry_year').val());
+	// Trigger the "change" event manually
+	var evt = document.createEvent('HTMLEvents');
+	evt.initEvent('change', false, true);
+	document.getElementById('expiry-date').dispatchEvent(evt);
+});
+
+
 var card = new Card({
 	// a selector or DOM element for the form where users will
 	// be entering their information
@@ -8,12 +18,12 @@ var card = new Card({
 
 	formSelectors: {
 		numberInput: 'input#id_credit_card_number', // optional — default input[name="number"]
-		// expiryInput: 'input#expiry', // optional — default input[name="expiry"]
+		expiryInput: 'input#expiry-date',// optional — default input[name="expiry"]
 		cvcInput: 'input#id_cvv', // optional — default input[name="cvc"]
-		// nameInput: 'input#name' // optional - defaults input[name="name"]
+		nameInput: 'input#id_name' // optional - defaults input[name="name"]
 	},
 
-	width: 200, // optional — default 350px
+	width: 300, // optional — default 350px
 	formatting: true, // optional - default true
 
 	// Strings for translation - optional
@@ -26,7 +36,7 @@ var card = new Card({
 	placeholders: {
 		number: '•••• •••• •••• ••••',
 		// name: 'Full Name',
-		// expiry: '••/••',
+		expiry: '••/••••',
 		cvc: '•••'
 	},
 
@@ -35,5 +45,5 @@ var card = new Card({
 	},
 
 	// if true, will log helpful messages for setting up Card
-	debug: false // optional - default false
+	debug: true // optional - default false
 });
